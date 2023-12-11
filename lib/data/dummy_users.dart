37e05c379768630c650c059cf360d3ca1429d4e2
EduardoSilva03 <eduaardo.silva03@gmail.com>
2023-12-11 17:54:34 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': User(
+    id: '1',
+    name: 'Eduardo',
+    email: 'eduardo@gmail.com',
+  ),
+  '2': User(
+    id: '2',
+    name: 'Leonardo',
+    email: 'leonardo@hotmail.com',
+  ),
+  '3': User(
+    id: '3',
+    name: 'Gustavo',
+    email: 'gustavo@yahoo.com',
+  ),
+  '4': User(
+    id: '4',
+    name: 'Lucas',
+    email: 'lucas@outlook.com',
+  )
+};
